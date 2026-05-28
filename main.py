668869c9397b0a9ed67483e5ea399bf22e1209cf
ech00f <consollerhahahaha@gmail.com>
2026-05-28 22:17:49 +0300

    add some base for automessage; add env support

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello woprld!!!")
+# from pyrogram import *
+from dotenv import load_dotenv
+import asyncio
+import os
+
+# load_dotenv()
+
+# app = Client("AwesomeSetup", api_id=os.getenv("API_ID"), api_hash=os.getenv("API_HASH"))
+
+
+# @app.on_message(filters.private)
+# async def hello(client, message):
+#     await message.reply("hi, this is auto message")
+
+
+# app.run()
