cea362dc970af7ec5439c7f34e14cfa964383a5f
ech00f <consollerhahahaha@gmail.com>
2026-05-29 00:17:34 +0300

    solve the problem with not media played

--- configs/telegram_config.py
+++ configs/telegram_config.py
@@ -11,4 +11,8 @@ async def get_tgbio_text():
     # this text will be inserted into your bio when you update music or something similar. The maximum text without premium telegram is 70 CHARACTERS, with a subscription - 140 CHARACTERS.
     # get_now_playing_info returns "app" (spotify.exe, chrome.exe like this), "artist" and "music title" 
     app, artist, title = await get_now_playing_info()
+    
+    if not app and not artist and not title:
+        return f"Nothing right now, but it's not forever"
+    
     return f'^_~ NOW PLAYING - "{artist} - {title}"'[:70]
